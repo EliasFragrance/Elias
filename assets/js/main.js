afc7280f6c469d56463d608a9ea909275b8c994d
Parfums de Elias <alexander.rook@yahoo.ca>
2025-12-24 23:47:18 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
+// active nav highlight + simple reveal animation
+(function(){
+  const path = location.pathname.split("/").pop() || "index.html";
+  document.querySelectorAll("[data-nav]").forEach(a=>{
+    if(a.getAttribute("href") === path) a.classList.add("active");
+  });
+
+  const els = document.querySelectorAll(".fade-up");
+  const io = new IntersectionObserver((entries)=>{
+    entries.forEach(e=>{
+      if(e.isIntersecting) e.target.classList.add("in");
+    });
+  }, { threshold: 0.12 });
+
+  els.forEach(el=>io.observe(el));
+})();
 
